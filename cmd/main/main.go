dd8e3246e03e3e503d68ac0555b0a760b4123f69
Navnoor Singh Bal <nbbal2003@gmal.com>
2024-07-06 05:49:19 +0530

    Add User and get Users setup

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"jwt-app/pkg/database"
 	"jwt-app/pkg/initializers"
+	"jwt-app/pkg/routes"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func init() {
@@ -18,16 +22,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", store)
-	// // Setting up a new router for our routes to api
-	// mux := mux.NewRouter()
-	// // Registering routes
-	// routes.RegisterUserRotues(mux)
-	// http.Handle("/", mux)
+	fmt.Printf("Database: %+v\n", store)
 
-	// // Starting server
-	// fmt.Println("Starting server at port 8000")
-	// if err := http.ListenAndServe(":8000", mux); err != nil {
-	// 	panic(err)
-	// }
+	// Setting up SQL Table
+	if err := store.CreateTable(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Setting up a new router for our routes to api
+	mux := mux.NewRouter()
+	// Registering routes
+	routes.RegisterUserRotues(mux, *store)
+	http.Handle("/", mux)
+
+	// Starting server
+	fmt.Println("Starting server at port 8000")
+	if err := http.ListenAndServe(":8000", mux); err != nil {
+		panic(err)
+	}
 }
