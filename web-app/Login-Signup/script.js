f2dfa8a74f19d0509cbc29100e30b1f54220054a
Navnoor Singh Bal <nbbal2003@gmal.com>
2024-07-07 16:37:46 +0530

    Added sign up web

--- web-app/Login-Signup/script.js
+++ web-app/Login-Signup/script.js
@@ -4,6 +4,8 @@ const signInLink = document.querySelector('.signIn-link');
 const signInForm = document.querySelector('.sign-in');
 const signUpForm = document.querySelector('.sign-up');
 
+const apiLink = "https://critical-tobe-student-iitropar-6d8618df.koyeb.app"
+
 signUpLink.addEventListener('click', () => {
     wrapper.classList.add('signup-clicking');
     wrapper.classList.remove('signin-clicking');
@@ -13,3 +15,25 @@ signInLink.addEventListener('click', () => {
     wrapper.classList.add('signin-clicking');
     wrapper.classList.remove('signup-clicking');
 });
+
+function onSignUp() {
+    alert("SignUping");
+    const reqOptions = {
+        mode: 'no-cors',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: document.getElementById('Name').value,
+            email: document.getElementById('Email').value,
+            password: document.getElementById('Password').value,
+        })
+    }
+    fetch(apiLink + '/signup', reqOptions)
+    .then(response => response.json())
+    .then((data)=> {
+        console.log(data);
+        alert("SignUp Successful");
+    })
+}
